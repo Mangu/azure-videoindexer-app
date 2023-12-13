ebbf9db7509cfae3f3baa2b7cd039d8d0f3ec0c1
Danilo Diaz <ddiaz@live.com>
2023-12-12 21:59:38 -0500

    added chat bubble

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -2,8 +2,9 @@
   <div class="page-container">
     <h3>Chat</h3>
     <div class="messages">
-      <div v-for="(message, index) in messages" :key="index" class="form-group">
-        {{ message }}
+      <div v-for="(message, index) in messages" :key="index" class="form-group chat-bubble">
+        <p>{{ message }}</p>
+        <span class="sender">user</span>
       </div>
     </div>    
     <div class="form-group">
@@ -53,4 +54,19 @@
     flex: 1;
     overflow-y: auto; /* This will add a scrollbar if the messages overflow the div */
   }
+  .chat-bubble {
+  background-color: #f0f0f0;
+  border-radius: 20px;
+  padding: 10px;
+  margin: 10px 0;
+  position: relative;
+}
+.sender {
+  font-size: 0.8em;
+  color: #888;
+  position: absolute;
+  bottom: 5px;
+  right: 10px;
+  font-weight: bold;
+}
 </style>
